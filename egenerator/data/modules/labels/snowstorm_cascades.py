165fe77fec65def710df803262e7f4721f34381d
mhuen <mhuen@users.noreply.github.com>
2020-05-18 14:03:49 +0200

    Update Casacde Extension: this will break older models, expect devaition of about 0.5 meter and 2 ns

--- egenerator/data/modules/labels/snowstorm_cascades.py
+++ egenerator/data/modules/labels/snowstorm_cascades.py
@@ -247,14 +247,25 @@ class SnowstormCascadeGeneratorLabelModule(BaseComponent):
             Shifted vertex position (position of shower maximum) in meter and
             shifted vertex time in nano seconds.
         """
+
+        # Radiation length in meters, assuming an ice density of 0.9216 g/cm^3
+        l_rad = (0.358/0.9216)  # in meter
+
         c = 0.299792458  # meter / ns
         dir_x = -np.sin(zenith) * np.cos(azimuth)
         dir_y = -np.sin(zenith) * np.sin(azimuth)
         dir_z = -np.cos(zenith)
 
-        a = 2.03 + 0.604 * np.log(ref_energy + eps)
-        b = 0.633
-        l_rad = (0.358/0.9216)  # in meter
+        """
+        Parameters taken from I3SimConstants (for particle e-):
+        https://code.icecube.wisc.edu/projects/icecube/browser/IceCube/
+        meta-projects/combo/trunk/sim-services/private/
+        sim-services/I3SimConstants.cxx
+        """
+        a = 2.01849 + 0.63176 * np.log(ref_energy + eps)
+        b = l_rad/0.63207
+
+        # Mode of the gamma distribution gamma_dist(a, b) is: (a-1.)/b
         length_to_maximum = np.clip(((a-1.)/b)*l_rad, 0., float('inf'))
         x_shifted = x + dir_x * length_to_maximum
         y_shifted = y + dir_y * length_to_maximum
