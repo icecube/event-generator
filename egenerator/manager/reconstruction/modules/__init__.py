a2557957ed88c66211a8de387be07ed1dc74b18d
mhuen <mhuen@users.noreply.github.com>
2020-08-15 18:42:18 +0200

    test goodness of fit

--- egenerator/manager/reconstruction/modules/__init__.py
+++ egenerator/manager/reconstruction/modules/__init__.py
@@ -1,5 +1,6 @@
 from .reconstruction import Reconstruction, SelectBestReconstruction
 from .covariance import CovarianceMatrix
+from .fit_quality import GoodnessOfFit
 from .opening_angle import CircularizedAngularUncertainty
 from .mcmc import MarkovChainMonteCarlo
 from .visualization import Visualize1DLikelihoodScan
@@ -8,6 +9,7 @@ __all__ = [
     'Reconstruction',
     'SelectBestReconstruction',
     'CovarianceMatrix',
+    'GoodnessOfFit',
     'CircularizedAngularUncertainty',
     'MarkovChainMonteCarlo',
     'Visualize1DLikelihoodScan',
