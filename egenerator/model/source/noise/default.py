6ed1a68c08f92dd4c02d98ebba53bb58f32c5140
mhuen <mhuen@users.noreply.github.com>
2020-10-06 13:20:10 +0200

    add support for time window exclusions

--- egenerator/model/source/noise/default.py
+++ egenerator/model/source/noise/default.py
@@ -184,6 +184,10 @@ class DefaultNoiseModel(Source):
             # accumulate time window exclusions for each event
             # shape: [n_batch]
             tf.print(tw_cdf_exclusion, 'noise: tw_cdf_exclusion')
+            tf.print(dom_pdf_constant, 'noise: dom_pdf_constant')
+            tf.print(tw_cdf_exclusion.shape, 'noise: tw_cdf_exclusion')
+            tf.print(dom_pdf_constant.shape, 'noise: dom_pdf_constant')
+            tf.print(x_time_excl_batch_id.shape, 'noise: x_time_excl_batch_id')
             event_cdf_exclusion = tf.tensor_scatter_nd_add(
                 tf.zeros_like(dom_pdf_constant),
                 indices=x_time_excl_batch_id,
