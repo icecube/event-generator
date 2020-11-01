608a384e50106e3034a851abedc7582fa96e26d8
mhuen <mhuen@users.noreply.github.com>
2020-11-01 19:34:02 +0100

    add multi-cascade

--- egenerator/reconstruct_test_data.py
+++ egenerator/reconstruct_test_data.py
@@ -131,8 +131,10 @@ def main(config_files, reco_config_file=None):
         if not os.path.exists(os.path.join(manager_dir, 'configuration.yaml')):
             msg = 'Could not find a saved model at {!r}!'
             raise ValueError(msg.format(manager_dir))
+        print('Restoring Manager from file')
 
     else:
+        print('Rebuilding Manager from scratch (make sure to load models...)')
         if len(seed_names) > 0:
             # Model Manager is being built from scratch, so we need to pass
             # the data handler settings with the modified misc module
@@ -147,20 +149,13 @@ def main(config_files, reco_config_file=None):
             }
 
     # load models from config files
-    if 'restore_manager' in reco_config:
-        print('Restoring Manager from file')
-        restore_manager = reco_config['restore_manager']
-    else:
-        print('Rebuilding Manager from scratch (hopefully loading models)')
-        restore_manager = False
-
     models = []
     for config_file in config_files:
 
         # load manager objects and extract models and a data_handler
         model_manger,  _, data_handler, data_transformer = build_manager(
             SetupManager([config_file]).get_config(),
-            restore=restore_manager,
+            restore=manager_config['restore_model'],
             modified_sub_components=deepcopy(modified_sub_components),
             allow_rebuild_base_sources=False,
         )
