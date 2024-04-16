0482dc15db75eca59017325bc9aeb60bdb00646e
mhuen <mhuen@users.noreply.github.com>
2024-04-16 12:25:06 +0200

    Add pre-commit hook; update setup

--- egenerator/__about__.py
+++ egenerator/__about__.py
@@ -1,5 +1,16 @@
-__version__ = "1.0.3-dev"
 __author__ = "Mirco Huennefeld"
 __author_email__ = "mirco.huennefeld@tu-dortmund.de"
 __description__ = "IceCube Event-Generator"
-__url__ = "https://github.com/mhuen/event-generator"
+__url__ = "https://github.com/icecube/event-generator"
+
+__version_major__ = 1
+__version_minor__ = 0
+__version_patch__ = 3
+__version_info__ = "-dev"
+
+__version__ = "{}.{}.{}{}".format(
+    __version_major__,
+    __version_minor__,
+    __version_patch__,
+    __version_info__,
+)
