a5584e97a801eac418fc358e465d438902ab6cd9
mhuen <mhuen@users.noreply.github.com>
2024-06-05 16:52:08 +0200

    Only divide by total charge; update configs

--- egenerator/loss/default.py
+++ egenerator/loss/default.py
@@ -200,19 +200,7 @@ class DefaultLossModule(BaseComponent):
 
         if normalize_by_total_charge:
             total_charge = tf.reduce_sum(data_batch_dict["x_pulses"][:, 0])
-            batch_size = tf.cast(
-                tf.shape(data_batch_dict["x_dom_charge"])[0],
-                dtype=total_charge.dtype,
-            )
-
-            # Note: this depends on the actual loss being used.
-            # Here we assume that an additional poisson term for each DOM
-            # is used as well. This adds ~5160 (minus exclusions) additional
-            # terms to the likelhood.
-            # ToDo: properly count number of likelihood terms and also properly
-            # normalize if loss is composed from multiple individual losses
-            approx_number_of_terms = total_charge + 5160 * batch_size
-            loss_terms = [loss / approx_number_of_terms for loss in loss_terms]
+            loss_terms = [loss / total_charge for loss in loss_terms]
 
         if reduce_to_scalar:
             return tf.math.add_n(
