c128d8e040f460334475084b44d3752c3c87f80f
mhuen <mhuen@users.noreply.github.com>
2024-09-07 11:55:53 -0500

    Bump version number for release

--- egenerator/__about__.py
+++ egenerator/__about__.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/icecube/event-generator"
 
 __version_major__ = 1
 __version_minor__ = 0
-__version_patch__ = 3
-__version_info__ = "-dev"
+__version_patch__ = 4
+__version_info__ = ""
 
 __version__ = "{}.{}.{}{}".format(
     __version_major__,
