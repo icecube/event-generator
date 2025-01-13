Charge PDF: fix calculation of PDF over components

egenerator/loss/default.py
@@ -145,7 +145,7 @@ class DefaultLossModule(BaseComponent):
             This  dictionary must at least contain:
 
                 'dom_charges': the predicted charge at each DOM
-                               Shape: [-1, 86, 60, 1]
+                               Shape: [-1, 86, 60]
                 'pulse_pdf': The likelihood evaluated for each pulse
                              Shape: [-1]
         tensors : DataTensorList
@@ -283,7 +283,7 @@ class DefaultLossModule(BaseComponent):
             This  dictionary must at least contain:
 
                 'dom_charges': the predicted charge at each DOM
-                               Shape: [-1, 86, 60, 1]
+                               Shape: [-1, 86, 60]
                 'pulse_pdf': The likelihood evaluated for each pulse
                              Shape: [-1]
         tensors : DataTensorList
@@ -322,7 +322,7 @@ class DefaultLossModule(BaseComponent):
 
         # shape: [n_batch, 86, 60]
         dom_charges_true = tf.squeeze(hits_true, axis=-1)
-        dom_charges_pred = tf.squeeze(result_tensors["dom_charges"], axis=-1)
+        dom_charges_pred = result_tensors["dom_charges"]
 
         # throw error if this is being used with time window exclusions
         # one needs to calculate cumulative pdf from exclusion window and
@@ -664,7 +664,7 @@ class DefaultLossModule(BaseComponent):
             This  dictionary must at least contain:
 
                 'dom_charges': the predicted charge at each DOM
-                               Shape: [-1, 86, 60, 1]
+                               Shape: [-1, 86, 60]
                 'dom_charges_pdf': charge likelihood evaluated for each DOM
                                Shape: [-1, 86, 60]
                 'pulse_pdf': The likelihood evaluated for each pulse
@@ -1016,7 +1016,7 @@ class DefaultLossModule(BaseComponent):
             This  dictionary must at least contain:
 
                 'dom_charges': the predicted charge at each DOM
-                               Shape: [-1, 86, 60, 1]
+                               Shape: [-1, 86, 60]
                 'dom_charges_variance':
                     the predicted variance on the charge at each DOM.
                     This assumes the underlying distribution is a negative
@@ -1049,10 +1049,8 @@ class DefaultLossModule(BaseComponent):
 
         # shape: [n_batch, 86, 60]
         hits_true = tf.squeeze(data_batch_dict["x_dom_charge"], axis=-1)
-        hits_pred = tf.squeeze(result_tensors["dom_charges"], axis=-1)
-        dom_charges_variance = tf.squeeze(
-            result_tensors["dom_charges_variance"], axis=-1
-        )
+        hits_pred = result_tensors["dom_charges"]
+        dom_charges_variance = result_tensors["dom_charges_variance"]
 
         # compute over-dispersion factor alpha
         # var = mu + alpha*mu**2
@@ -1151,7 +1149,7 @@ class DefaultLossModule(BaseComponent):
             This  dictionary must at least contain:
 
                 'dom_charges': the predicted charge at each DOM
-                               Shape: [-1, 86, 60, 1]
+                               Shape: [-1, 86, 60]
                 'dom_charges_variance':
                     the predicted variance on the charge at each DOM.
                     This assumes the underlying distribution is a negative
@@ -1184,10 +1182,8 @@ class DefaultLossModule(BaseComponent):
 
         # shape: [n_batch, 86, 60]
         hits_true = tf.squeeze(data_batch_dict["x_dom_charge"], axis=-1)
-        hits_pred = tf.squeeze(result_tensors["dom_charges"], axis=-1)
-        dom_charges_variance = tf.squeeze(
-            result_tensors["dom_charges_variance"], axis=-1
-        )
+        hits_pred = result_tensors["dom_charges"]
+        dom_charges_variance = result_tensors["dom_charges_variance"]
 
         # throw error if this is being used with time window exclusions
         # one needs to calculate cumulative pdf from exclusion window and
@@ -1299,7 +1295,7 @@ class DefaultLossModule(BaseComponent):
             This  dictionary must at least contain:
 
                 'dom_charges': the predicted charge at each DOM
-                               Shape: [-1, 86, 60, 1]
+                               Shape: [-1, 86, 60]
         tensors : DataTensorList
             The data tensor list describing the input data
         sort_loss_terms : bool, optional
@@ -1326,7 +1322,7 @@ class DefaultLossModule(BaseComponent):
 
         # shape: [n_batch, 86, 60]
         hits_true = tf.squeeze(data_batch_dict["x_dom_charge"], axis=-1)
-        hits_pred = tf.squeeze(result_tensors["dom_charges"], axis=-1)
+        hits_pred = result_tensors["dom_charges"]
 
         # throw error if this is being used with time window exclusions
         # one needs to calculate cumulative pdf from exclusion window and

egenerator/model/source/cascade/default.py
@@ -171,7 +171,17 @@ class DefaultCascadeModel(Source):
 
                 'dom_charges':
                     The predicted charge at each DOM
-                    Shape: [n_events, 86, 60, 1]
+                    Shape: [n_events, 86, 60]
+                'dom_charges_component':
+                    The predicted charge at each DOM for each component
+                    Shape: [n_events, 86, 60, n_components]
+                'dom_charges_variance':
+                    The predicted charge variance at each DOM
+                    Shape: [n_events, 86, 60]
+                'dom_charges_variance_component':
+                    The predicted charge variance at each DOM for each
+                    component of the mixture model.
+                    Shape: [n_events, 86, 60, n_components]
                 'dom_charges_pdf':
                     The charge likelihood evaluated for each DOM.
                     Shape: [n_events, 86, 60]
@@ -428,8 +438,6 @@ class DefaultCascadeModel(Source):
 
             # extend to correct batch shape:
             dom_coords = tf.ones_like(dx_normed) * dom_coords
-
-            print("\t dom_coords", dom_coords)
             input_list.append(dom_coords)
 
         if config["num_local_vars"] > 0:
@@ -438,8 +446,6 @@ class DefaultCascadeModel(Source):
             local_vars = (
                 tf.ones_like(dx_normed) * self._untracked_data["local_vars"]
             )
-            print("\t local_vars", local_vars)
-
             input_list.append(local_vars)
 
         x_doms_input = tf.concat(input_list, axis=-1)
@@ -550,7 +556,19 @@ class DefaultCascadeModel(Source):
         if n_components < 1:
             raise ValueError("{!r} !> 0".format(n_components))
 
-        print("\t Charge method:", config["charge_distribution_type"])
+        # print out information about the mixture model components
+        print("\t Charge PDF mixture model components:")
+        decoder_config = self.decoder_charge.configuration.config["config"]
+        if "decoder_names" in self.decoder_charge._untracked_data:
+            counter = 0
+            for name in self.decoder_charge._untracked_data["decoder_names"]:
+                base_name, num, weight = decoder_config["decoder_mapping"][
+                    name
+                ]
+                print(f"\t\t {base_name}: {num} [w={weight}]")
+        else:
+            print(f"\t\t Decoder name: {self.decoder_charge.name}")
+
         print("\t Time PDF mixture model components:")
         decoder_config = self.decoder.configuration.config["config"]
         counter = 0
@@ -666,17 +684,19 @@ class DefaultCascadeModel(Source):
 
         # check that things are spelled correctly
         for param_name in config["charge_scale_tensors"]:
-            if param_name not in self.decoder.loc_parameters:
+            if param_name not in self.decoder_charge.loc_parameters:
                 raise ValueError(
                     f"Charge scale tensor {param_name} not found in "
-                    f"decoder loc_parameters: {self.decoder.loc_parameters}!"
+                    f"decoder loc_parameters: {self.decoder_charge.loc_parameters}!"
                 )
 
         # transform charge scale tensors
         latent_vars_charge_scaled = []
         for idx, param_name in enumerate(self.decoder_charge.parameter_names):
 
-            charge_tensor = latent_vars_charge[..., idx]
+            charge_tensor = tf.expand_dims(
+                latent_vars_charge[..., idx], axis=-1
+            )
 
             if param_name in self.decoder_charge.loc_parameters:
 
@@ -695,11 +715,7 @@ class DefaultCascadeModel(Source):
                 # clip value range for more stability during training
                 # apply exponential which also forces positive values
                 charge_tensor = tf.exp(
-                    tf.clip_by_value(
-                        tf.expand_dims(charge_tensor, axis=-1),
-                        -20.0,
-                        15,
-                    )
+                    tf.clip_by_value(charge_tensor, -20.0, 15)
                 )
 
                 if param_name in config["charge_scale_tensors"]:
@@ -719,18 +735,28 @@ class DefaultCascadeModel(Source):
         latent_vars_charge = tf.concat(latent_vars_charge_scaled, axis=-1)
         tensor_dict["latent_vars_charge"] = latent_vars_charge
 
+        tensor_dict["dom_charges_component"] = self.decoder_charge.expectation(
+            latent_vars=latent_vars_charge,
+            reduce_components=False,
+        )
         tensor_dict["dom_charges"] = self.decoder_charge.expectation(
             latent_vars=latent_vars_charge,
+            reduce_components=True,
+        )
+        tensor_dict["dom_charges_variance_component"] = (
+            self.decoder_charge.variance(
+                latent_vars=latent_vars_charge, reduce_components=False
+            )
         )
         tensor_dict["dom_charges_variance"] = self.decoder_charge.variance(
-            latent_vars=latent_vars_charge,
+            latent_vars=latent_vars_charge, reduce_components=True
         )
 
         # -------------------------
         # Compute charge PDF values
         # -------------------------
         dom_charges_pdf = self.decoder_charge.pdf(
-            x=dom_charges_true,
+            x=tf.squeeze(dom_charges_true, axis=3),
             latent_vars=latent_vars_charge,
             reduce_components=True,
         )

egenerator/model/source/noise/default.py
@@ -159,19 +159,19 @@ class DefaultNoiseModel(Source):
         # shape: [n_batch]
         livetime = time_window[:, 1] - time_window[:, 0]
 
-        # shape: [n_batch, 1, 1, 1]
+        # shape: [n_batch, 1, 1]
         livetime_exp = tf.reshape(
-            time_window[:, 1] - time_window[:, 0], [-1, 1, 1, 1]
+            time_window[:, 1] - time_window[:, 0], [-1, 1, 1]
         )
 
         # compute the expected charge at each DOM based off of noise rate
-        # shape: [1, 86, 60, 1]
-        dom_noise_rates = tf.reshape(
+        # shape: [1, 86, 60]
+        dom_noise_rates = np.reshape(
             detector.dom_noise_rates.astype(param_dtype_np),
-            shape=[1, 86, 60, 1],
+            [1, 86, 60],
         )
 
-        # shape: [n_batch, 86, 60, 1]
+        # shape: [n_batch, 86, 60]
         dom_charges = dom_noise_rates * livetime_exp
 
         # shape: [n_batch]
@@ -235,9 +235,7 @@ class DefaultNoiseModel(Source):
 
         # scale by time exclusions
         if time_exclusions_exist:
-            dom_charges *= (
-                1.0 - dom_cdf_exclusion[..., tf.newaxis] + self.epsilon
-            )
+            dom_charges *= 1.0 - dom_cdf_exclusion + self.epsilon
 
         # add small constant to make sure dom charges are > 0:
         dom_charges += self.epsilon
@@ -246,11 +244,14 @@ class DefaultNoiseModel(Source):
         # std = sqrt(var) = sqrt(mu + alpha*mu**2)
         dom_charges_variance = dom_charges + dom_charges_alpha * dom_charges**2
 
-        dom_charges_pdf = basis_functions.tf_log_negative_binomial(
-            x=tf.squeeze(data_batch_dict["x_dom_charge"], axis=3),
-            mu=dom_charges,
-            alpha=dom_charges_alpha,
-            dtype=self.configuration.config["config"]["float_precision"],
+        dom_charges_pdf = tf.math.exp(
+            basis_functions.tf_log_negative_binomial(
+                x=tf.squeeze(data_batch_dict["x_dom_charge"], axis=3),
+                mu=dom_charges,
+                alpha=dom_charges_alpha,
+                add_normalization_term=True,
+                dtype=self.configuration.config["config"]["float_precision"],
+            )
         )
 
         # Compute Log Likelihood for pulses
@@ -292,8 +293,12 @@ class DefaultNoiseModel(Source):
         # add tensors to tensor dictionary
         tensor_dict["time_offsets"] = None
         tensor_dict["dom_charges"] = dom_charges
+        tensor_dict["dom_charges_component"] = dom_charges[..., tf.newaxis]
         tensor_dict["dom_charges_pdf"] = dom_charges_pdf
         tensor_dict["dom_charges_variance"] = dom_charges_variance
+        tensor_dict["dom_charges_variance_component"] = dom_charges_variance[
+            ..., tf.newaxis
+        ]
         tensor_dict["pdf_constant"] = dom_pdf_constant
         tensor_dict["pdf_time_window"] = time_window
         tensor_dict["pulse_pdf"] = pulse_pdf

egenerator/utils/basis_functions.py
@@ -488,7 +488,7 @@ def _tf_evaluate_gamma(func_name, x, alpha, beta, replacement_value=0.0):
     return tf_helpers.double_where_trick_greater_zero(
         function=getattr(distribution, func_name),
         x=x,
-        cut_x_min=eps,
+        cut_x_min=tf.cast(eps, dtype=x.dtype),
         replacement_value=replacement_value,
     )
 
@@ -565,7 +565,7 @@ def gamma_pdf(x, alpha, beta, dtype=None):
     """
     x, alpha, beta = cast(dtype, x, alpha, beta)
     return np.where(
-        x <= 0,
+        x < 0,
         0.0,
         beta**alpha
         / special.gamma(alpha)
@@ -596,7 +596,7 @@ def gamma_log_pdf(x, alpha, beta, dtype=None):
     """
     x, alpha, beta = cast(dtype, x, alpha, beta)
     return np.where(
-        x <= 0,
+        x < 0,
         -np.inf,
         alpha * np.log(beta)
         - special.gammaln(alpha)
@@ -650,7 +650,7 @@ def gamma_cdf(x, alpha, beta, dtype=None):
         The Gamma CDF evaluated at x
     """
     x, alpha, beta = cast(dtype, x, alpha, beta)
-    return np.where(x <= 0, 0.0, special.gammainc(alpha, beta * x))
+    return np.where(x < 0, 0.0, special.gammainc(alpha, beta * x))
 
 
 def tf_gamma_ppf(q, alpha, beta, dtype=None):
@@ -843,7 +843,7 @@ def tf_poisson_pdf(x, mu, add_normalization_term=False, dtype=None):
     result = tf_helpers.double_where_trick_greater_zero(
         function=function,
         x=x,
-        cut_x_min=0,
+        cut_x_min=tf.cast(0, dtype=x.dtype),
         replacement_value=0.0,
     )
     return result
@@ -911,7 +911,10 @@ def tf_poisson_log_pdf(x, mu, add_normalization_term=False, dtype=None):
         return result
 
     result = tf_helpers.double_where_trick_greater_zero(
-        function=function, x=x, cut_x_min=0, replacement_value=-np.inf
+        function=function,
+        x=x,
+        cut_x_min=tf.cast(0, dtype=x.dtype),
+        replacement_value=-np.inf,
     )
     return result
 
@@ -1000,7 +1003,9 @@ def tf_log_negative_binomial(
     term1 = -inv_alpha * tf.math.log(1.0 + alpha_mu)
     term2 = x * tf.math.log(alpha_mu / (1.0 + alpha_mu))
 
-    return gamma_terms + term1 + term2
+    result = tf.where(x < 0, -np.inf, gamma_terms + term1 + term2)
+
+    return result
 
 
 def log_negative_binomial(
@@ -1063,7 +1068,9 @@ def log_negative_binomial(
     term1 = -inv_alpha * np.log(1.0 + alpha_mu)
     term2 = x * np.log(alpha_mu / (1.0 + alpha_mu))
 
-    return gamma_terms + term1 + term2
+    result = np.where(x < 0, -np.inf, gamma_terms + term1 + term2)
+
+    return result
 
 
 def convert_neg_binomial_params(mu, alpha_or_var, param_is_alpha):